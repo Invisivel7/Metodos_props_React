6d00bb86112fe9928e0abd4c617f6318d9009762
Ideolino Agostinho <ideolino2015@gmail.com>
2024-08-23 14:58:08 +0100

    UseState no React

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault() //Para o formulário não fazer o reload da página assim que o evento for chamado
-        console.log("cadastrou o usuário")
+        console.log(`O utilizador ${name} foi cadastrado com a senha ${password}`)
     }
+
+    //o name para uso do valor e o setName para alterar o valor
+    //Pode-se colocar um vaor por default dentro da função useState('Matheus')
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
     return(
         <div>
             <h1>Meu formulário de cadastro: </h1> 
@@ -11,11 +19,19 @@ function Form(){
             <form onSubmit={cadastrarUsuario}>
                 <div>
                     <label htmlFor="name">Nome: </label>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <input 
+                    type="text" 
+                    placeholder="Digite o seu nome"
+                    onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="password">Senha: </label>
-                    <input type="password" placeholder="Digite a sua senha" />
+                    <input 
+                    type="password" 
+                    placeholder="Digite a sua senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="cadastrar" />
